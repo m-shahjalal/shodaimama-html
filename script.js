aa34833fadfee867270b12700f238d2815be8087
shahjalal <lx.shahjalall@gmail.com>
2022-01-12 15:54:32 +0600

    static file ready

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('hello shodaimama');
+
+const images = []
+for (let i = 0; i <= 100; i++) {
+   const imageName = `imag_${i}`
+   import imageName from `../images/folder1/sadldf-000${i}`
+    images.push(imageName)
+}
